d7d2a16fd09e6ae94b6518a0cbf509adb107a313
Marvin Orendain <marvin.orendain@elis-mac-mini.mynetworksettings.com>
2024-03-29 11:54:29 -0400

    update grid component to manually define columns

--- src/App.js
+++ src/App.js
@@ -12,9 +12,33 @@ const GridData = () => {
   const rowData = data.arbitrage_data;
 
   // Column Definitions: Defines the columns to be displayed.
-  const colDefs = [];
-  const keys = Object.keys(data.arbitrage_data[0]);
-  keys.forEach(key => colDefs.push({ field: key }));
+  // const colDefs = [];
+  // automatically get column headers from json data
+  // const keys = Object.keys(data.arbitrage_data[0]);
+  // keys.forEach(key => colDefs.push({ field: key }));
+
+  // manually define column headers
+  const colDefs = [
+    { field: "bet_placed", headerName: "Bet Placed", width: 120 }, 
+    { field: "arb_percent", headerName: "Arb %", width: 100 },
+    { field: "market", filter: true },
+    { field: "game_id", hide: true },
+    { field: "sport", filter: true, width: 120 },
+    { field: "league", filter: true, width: 100 },
+    { field: "home_team", headerName: "Home" },
+    { field: "away_team", headerName: "Away" },
+    { field: "start_date", headerName: "Date", width: 180 }, 
+    { field: "best_price_home_name", hide: true },
+    { field: "best_price_home_odd", hide: true },
+    { field: "best_price_home_odd_books", hide: true },
+    { field: "best_price_away_name", hide: true },
+    { field: "best_price_away_odd", hide: true },
+    { field: "best_price_away_odd_books", hide: true },
+    { field: "oddsjam_price_home_odd", hide: true },
+    { field: "oddsjam_price_away_odd", hide: true },
+    { field: "type", hide: true },
+    { field: "is_live", headerName: "Live", width: 60 }
+  ];
   
   return (
     // wrapping container with theme & size
