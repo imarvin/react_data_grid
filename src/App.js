0750e3761da1b541e47921128fca8994af13cc7b
Marvin Orendain <marvin.orendain@elis-mac-mini.mynetworksettings.com>
2024-03-29 11:37:57 -0400

    use arbs data to populate ag-grid

--- src/App.js
+++ src/App.js
@@ -4,23 +4,25 @@ import { AgGridReact } from 'ag-grid-react'; // AG Grid Component
 import "ag-grid-community/styles/ag-grid.css"; // Mandatory CSS required by the grid
 import "ag-grid-community/styles/ag-theme-quartz.css"; // Optional Theme applied to the grid
 
+// local json data
+import data from './arbs.json';
+
 const GridData = () => {
   // Row Data: The data to be displayed.
-  const [rowData, setRowData] = useState([
-
-  ]);
+  const rowData = data.arbitrage_data;
 
   // Column Definitions: Defines the columns to be displayed.
-  const [colDefs, setColDefs] = useState([
-  ]);
-
+  const colDefs = [];
+  const keys = Object.keys(data.arbitrage_data[0]);
+  keys.forEach(key => colDefs.push({ field: key }));
+  
   return (
     // wrapping container with theme & size
     <div
       className="ag-theme-quartz" // applying the grid theme
-      style={{ height: 500 }} // the grid will fill the size of the parent container
+      style={{ height: 500, width: '100%' }} // the grid will fill the size of the parent container
     >
-      <AgGridReact
+      <AgGridReact style={{ width: '100%', height: '100%' }}
         rowData={rowData}
         columnDefs={colDefs}
       />
