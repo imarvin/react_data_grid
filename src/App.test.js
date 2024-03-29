d86feb5be3b0f37be44cb79b542ba9a21f42457a
Marvin Orendain <marvin.orendain@elis-mac-mini.mynetworksettings.com>
2024-03-29 12:48:15 -0400

    add simple unit test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,16 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('value formatter and cell renderer', async () => {
+  // First render the App component we wish to tests
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+  // Test the value formatter by searching for the correct string
+  expect(screen.getByText('Arb %')).toBeDefined();
+
+  // Now find the expected content of the cell renderer
+  const marketColumn = await screen.findByText('Market');
+  expect(marketColumn).toBeDefined();
+
 });
