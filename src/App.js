d75e9ae6dec44999b4ff31ddb74545125640e70c
Marvin Orendain <marvin.orendain@elis-mac-mini.mynetworksettings.com>
2024-03-29 11:23:53 -0400

    update app; add ag-grid component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import { AgGridReact } from 'ag-grid-react'; // AG Grid Component
+import "ag-grid-community/styles/ag-grid.css"; // Mandatory CSS required by the grid
+import "ag-grid-community/styles/ag-theme-quartz.css"; // Optional Theme applied to the grid
+
+const GridData = () => {
+  // Row Data: The data to be displayed.
+  const [rowData, setRowData] = useState([
+
+  ]);
+
+  // Column Definitions: Defines the columns to be displayed.
+  const [colDefs, setColDefs] = useState([
+  ]);
+
+  return (
+    // wrapping container with theme & size
+    <div
+      className="ag-theme-quartz" // applying the grid theme
+      style={{ height: 500 }} // the grid will fill the size of the parent container
+    >
+      <AgGridReact
+        rowData={rowData}
+        columnDefs={colDefs}
+      />
+    </div>
+  )
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <GridData />
     </div>
   );
 }
